45c41c83cad42406dd7c9a5cda583f8dd543c16a
Jason Yeomans <jyeo098@gmail.com>
2015-09-11 02:59:10 -0400

    .first

--- api/apiv1/views.py
+++ api/apiv1/views.py
@@ -189,11 +189,11 @@ class QueueGroupViewSet(viewsets.ModelViewSet):
         if 'played' in j:
             track.played = j['played']
         if 'vote' in j:
-            if j['vote'] > 0 and not track.voted_up.get(pk=listener.pk):
+            if j['vote'] > 0 and not track.voted_up.filter(pk=listener.pk).first():
                 track.rating += 1
                 track.voted_up.add(listener)
                 track.voted_down.remove(listener)
-            elif j['vote'] < 0 and not track.voted_down.get(pk=listener.pk):
+            elif j['vote'] < 0 and not track.voted_down.filter(pk=listener.pk).first():
                 track.rating -= 1
                 track.voted_down.add(listener)
                 track.voted_up.remove(listener)
